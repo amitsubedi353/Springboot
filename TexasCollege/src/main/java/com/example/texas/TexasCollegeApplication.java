d93613a0544e8a8a2fe3232a56bc52d040c7cbce
amitsubedi353 <45146882+amitsubedi353@users.noreply.github.com>
2022-12-02 11:52:22 +0545

    Add files via upload

---
+++ TexasCollege/src/main/java/com/example/texas/TexasCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.texas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexasCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexasCollegeApplication.class, args);
+	}
+
+}
---
+++ TexasCollege/src/test/java/com/example/texas/TexasCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.texas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexasCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
